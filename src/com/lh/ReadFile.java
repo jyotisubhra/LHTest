d6d247ac036fbc0805274c40ce7e77af3f9a4373
jyotisubhra <33259027+jyotisubhra@users.noreply.github.com>
2018-06-21 15:59:02 +0530

    getting filename: make it dynamic

--- src/com/lh/ReadFile.java
+++ src/com/lh/ReadFile.java
@@ -3,20 +3,36 @@ package com.lh;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ReadFile {
 
 	public static void main(String[] args) {
 		File file = new File("src/conf/inputParameters.properties");
+		File fileName = new File("src/conf/InputFileName.properties");
 
-		BufferedReader br;
+		BufferedReader br = null;
+		BufferedReader brFile = null;
+		String filename = null;
 		try {
 			br = new BufferedReader(new FileReader(file));
-			GenerateFile.generatedXML(br);
+			brFile = new BufferedReader(new FileReader(fileName));
+			//Get the filename
+			filename = brFile.readLine();
+			GenerateFile.generatedXML(br, filename);
+			
 			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			System.out.println("Error processing records");
 			e.printStackTrace();
+		} finally {			
+			try {
+				br.close();
+				brFile.close();
+			} catch (IOException e) {
+				System.out.println("Error closing Buffered Reader");
+				e.printStackTrace();
+			}
 		}
 
 		
